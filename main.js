f40e31093943a3b561ea77d3579e0811ffe83697
nev0rmind <49232731+nev0rmind@users.noreply.github.com>
2022-10-31 12:03:11 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// Copy into your console & adapt selector
+
+let parent = document.querySelector('.sticky').parentElement;
+
+while (parent) {
+    const hasOverflow = getComputedStyle(parent).overflow;
+    if (hasOverflow !== 'visible') {
+        console.log(hasOverflow, parent);
+    }
+    parent = parent.parentElement;
+}
